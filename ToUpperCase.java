5482d74bfd30afd904c09d08031fc62f58427480
Darwin Rodríguez <Darwinrv94@gmail.com>
2025-06-08 12:01:12 -0600

    Update ToUpperCase.java

--- ToUpperCase.java
+++ ToUpperCase.java
@@ -2,7 +2,7 @@ public class ToUpperCase {
     public static void main(String[] args) {
         if (args.length > 0) {
             String input = args[0];
-            String output = input.toUpperCase();
+            String output = "Hello " + input.toUpperCase();
             System.out.println(output);
         } else {
             System.out.println("Please provide a string as an XXXXXXXX coño e' tu madre");
